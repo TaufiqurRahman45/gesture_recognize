cfd42613ecbe00d608cc493cc35f983fa3690c1e
LAPTOP-6OLBG1GR\User <toufiqurrahman45@gmail.com>
2020-08-11 00:30:07 +0800

    gesture recognize UI

---
+++ gesture/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'design/home.html')
+
+def gesture(request):
+    return render(request, 'design/gesture.html')
