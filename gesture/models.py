443061184637fab8239c07c11067fb2f62b76e63
manjurulhoque <rumimanzurulhoque@gmail.com>
2020-09-27 00:13:44 +0600

    Gesture image model added

--- gesture/models.py
+++ gesture/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class GestureImage(models.Model):
+    image = models.ImageField(upload_to="gestures")
+    time = models.IntegerField()
